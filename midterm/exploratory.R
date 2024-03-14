4a36590d608d20188fb4cbe064f2c6da4a326a99
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-03-13 23:43:51 -0700

    add pdfs and r exploratory script

---
+++ midterm/exploratory.R
@@ -0,0 +1,20 @@
+# SFU ECON832 Midterm
+# Spring 2024
+
+# Libraries
+
+library(readr)
+library(dplyr)
+library(here)
+
+# Load the data
+
+attributes_raw <- read_csv(here("data/simulated-data/midterm_simulated_market_data_x.csv"),
+                           show_col_types = F)
+
+
+blp_original_product_data <- read_csv("C:/Users/user/Documents/GitHub/julia-blp-modified/data and random draws/BLP_product_data.csv",
+                                      show_col_types = F)
+
+
+blp_original_product_data
