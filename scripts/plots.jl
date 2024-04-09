af37d05ccf2b25ea54d95a13982763a991484d7b
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-04-09 15:43:11 -0700

    update

--- scripts/plots.jl
+++ scripts/plots.jl
@@ -11,3 +11,15 @@ x = 1:10; y = rand(10); # These are the plotting data
 # Plot the data
 
 plot(x, y, label="my label")
+
+# For TidierPlots (ggplot2 implementation in Julia)
+
+using TidierPlots
+using DataFrames
+using PalmerPenguins
+
+penguins = dropmissing(DataFrame(PalmerPenguins.load()))
+
+ggplot(data = penguins) + 
+    geom_bar(@aes(x = species)) +
+    labs(x = "Species")
